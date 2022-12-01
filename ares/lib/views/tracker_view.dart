7d13d206d12c068b28bd7f70ba85a614f9dcfc3a
giftmischer69 <giftmischer69@protonmail.com>
2022-12-01 13:02:11 +0100

    canCheck feature start :heavy_check_mark:

--- ares/lib/views/tracker_view.dart
+++ ares/lib/views/tracker_view.dart
@@ -82,16 +82,17 @@ class _DailyTrackerViewState extends State<DailyTrackerView> {
     return result;
   }
 
-  int calculateStreak(List<TrackerRecord> records) {
-    if (records.isEmpty) {
+  int calculateStreak(List<TrackerRecord> trackerRecords) {
+    if (trackerRecords.isEmpty) {
       print("streak is 0, no records");
       return 0;
     }
 
     var today = DateTime.now().intoDay();
-    int recordsToday =
-        records.where((element) => element.timeStamp.isAfter(today)).length;
-    int recordsYesterday = records
+    int recordsToday = trackerRecords
+        .where((element) => element.timeStamp.isAfter(today))
+        .length;
+    int recordsYesterday = trackerRecords
         .where((element) =>
             element.timeStamp.isAfter(today.subtract(const Duration(days: 1))))
         .length;
@@ -102,16 +103,16 @@ class _DailyTrackerViewState extends State<DailyTrackerView> {
       return 0;
     }
 
-    records.sort((a, b) => a.timeStamp.compareTo(b.timeStamp));
-    records = records.reversed.toList();
+    trackerRecords.sort((a, b) => a.timeStamp.compareTo(b.timeStamp));
+    trackerRecords = trackerRecords.reversed.toList();
 
     int streak = 1;
     // if the latest record was at yesterdays date
     // https://stackoverflow.com/a/70727527/16357605
-    for (final pair in IterableZip([records, records.sublist(1)])) {
+    for (final pair
+        in IterableZip([trackerRecords, trackerRecords.sublist(1)])) {
       // pair[0] = index
       // pair[1] = entry before index
-
       if (!isDuringDayBefore(pair[0].timeStamp, pair[1].timeStamp)) {
         return streak;
       }
@@ -120,22 +121,74 @@ class _DailyTrackerViewState extends State<DailyTrackerView> {
     return streak;
   }
 
+  bool calculateCanCheck(List<TrackerRecord> trackerRecords) {
+    if (trackerRecords.isEmpty) {
+      return true;
+    }
+
+    var today = DateTime.now().intoDay();
+    int recordsToday = trackerRecords
+        .where((element) => element.timeStamp.isAfter(today))
+        .length;
+
+    if (recordsToday == 0) {
+      return true;
+    } else if (recordsToday == 1) {
+      return false;
+    } else if (recordsToday > 1) {
+      throw Exception(
+          "more than one record for today, recordsToday:$recordsToday");
+    } else {
+      throw Exception("should never be reached, recordsToday:$recordsToday");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO add record functionality
     // TODO add CircularProgressIndicator around left icon
     // TODO toggle right (and left) icon when a record for today is present
     // TODO detail task page, with description and editing etc
     // TODO add tasks somehow with different screen
+    // TODO Statistics View for DailyWidget with https://pub.dev/packages/table_calendar weekly/monthly views
 
     var trackerRecords = getTrackerRecords(context);
 
     var streak = calculateStreak(trackerRecords);
+    var canCheck = calculateCanCheck(trackerRecords);
 
     return ListTile(
-      leading: const Icon(Icons.calendar_month),
+      leading: Stack(
+        children: <Widget>[
+          Container(
+            width: 50,
+            height: 50,
+            decoration: BoxDecoration(
+              color: canCheck ? Colors.transparent : Colors.deepOrangeAccent,
+              shape: BoxShape.circle,
+            ),
+          ),
+          Container(
+            child: Icon(
+              Icons.calendar_month,
+              size: 40,
+              color: Colors.black,
+            ),
+            margin: EdgeInsets.all(5),
+          ),
+          // Circle Around
+          Container(
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(color: Colors.white)),
+            child: Icon(
+              Icons.check,
+              color: Colors.white,
+            ),
+          )
+        ],
+      ),
       title: Text(widget.tracker.title),
-      subtitle: Text("streak: $streak 🔥"),
+      subtitle: Text("Streak: $streak Days 🔥"),
       trailing: SizedBox(
         width: 100,
         child: Row(
@@ -167,7 +220,11 @@ class _DailyTrackerViewState extends State<DailyTrackerView> {
             ),
             IconButton(
               onPressed: () {
-                print("TODO ADD RECORD ${widget.tracker.id}");
+                if (!canCheck) {
+                  print("can't check!");
+                  return;
+                }
+
                 var record = TrackerRecord()
                   ..timeStamp = DateTime.now()
                   ..parent.value = widget.tracker;
@@ -175,25 +232,18 @@ class _DailyTrackerViewState extends State<DailyTrackerView> {
                 context.read<TrackerRecordProvider>().addRecord(record);
                 widget.notifyParent();
               },
-              icon: const Icon(Icons.add),
+              icon: canCheck ? Icon(Icons.add) : Icon(Icons.check),
             ),
             IconButton(
               onPressed: () {
                 // TODO also delete all records for this tracker
                 // when tracker is deleted
 
-                var thisTrackerRecords = context
-                    .read<TrackerRecordProvider>()
-                    .records
-                    .where((element) => element.parent.value != null)
-                    .where((element) =>
-                        element.parent.value!.id == widget.tracker.id);
+                context.read<TrackerProvider>().deleteTracker(widget.tracker);
 
-                for (var element in thisTrackerRecords) {
+                for (var element in trackerRecords) {
                   context.read<TrackerRecordProvider>().deleteRecord(element);
                 }
-
-                context.read<TrackerProvider>().deleteTracker(widget.tracker);
 
                 widget.notifyParent();
               },
