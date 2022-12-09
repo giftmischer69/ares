3c30aa9c1df9a413fdf51f2f08fcd8aa69e3d754
giftmischer69 <giftmischer69@protonmail.com>
2022-12-09 01:45:57 +0100

    CircularSegmentedProgressIndicator progress :bulb:

--- ares/lib/widgets/circular_segmented_progress_indicator.dart
+++ ares/lib/widgets/circular_segmented_progress_indicator.dart
@@ -11,18 +11,45 @@ class CircularSegmentedProgressIndicator extends StatelessWidget {
     // list bool for checked each day of the week
     // 0 -> monday
     // https://github.com/thisiskhan/thebrio_segmented_circle_border/blob/main/lib/thebri_segmented_circle_border.dart
-    var checkedDays = <bool>[true, false, false, true, false, true, false];
+    var checkedDays = <bool>[true, true, true];
+
+    var numberOfSegments = checkedDays.length;
+
+    var seperatorValues = <double>[];
+    var indicatorColors = <Color>[];
+    var indicatorValues = <double>[];
+
+    // 3 + 1 = 4
+    var lastValue = 0.0;
+    for (var isChecked in checkedDays) {
+      var seperatorValue = (1 / (numberOfSegments + 1)) / 3 + lastValue;
+      var indicatorValue = (1 / (numberOfSegments + 1)) + seperatorValue;
+      lastValue = indicatorValue;
+
+      seperatorValues.add(seperatorValue);
+      indicatorColors.add(isChecked ? Colors.green : Colors.grey);
+      indicatorValues.add(indicatorValue);
+    }
+
     int counter = 0;
     return Stack(
       alignment: Alignment.center,
       children: <Widget>[
-        for (var index in checkedDays)
-          CircularProgressIndicator(
-            value: (7 - counter) / 7,
-            color: index
-                ? Colors.yellow.withGreen(((255 / 7) * (7 - counter++)).toInt())
-                : Colors.transparent,
-            backgroundColor: Colors.transparent,
+        for (var index in List.generate(numberOfSegments, (i) => i).reversed)
+          Stack(
+            alignment: Alignment.center,
+            children: <Widget>[
+              CircularProgressIndicator(
+                value: indicatorValues[index],
+                color: indicatorColors[index],
+                backgroundColor: Colors.transparent,
+              ),
+              CircularProgressIndicator(
+                value: seperatorValues[index],
+                color: Colors.white,
+                backgroundColor: Colors.transparent,
+              ),
+            ],
           )
       ],
     );
